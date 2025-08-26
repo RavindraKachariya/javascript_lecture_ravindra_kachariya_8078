752d85b55789dcf54eebcc5144b16adab02a027a
Ravindra Kachariya <ravindrakachariya@gmail.com>
2025-08-26 10:31:29 +0530

    Add | Js

---
+++ Lms Project/PR - 1 Basic calculation/script.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 20
+
+console.log("Addition:", a + b)
+console.log("Multiplication:", a * b)
+console.log("Division:", a / b)
+console.log("Subtraction:", a - b)
+console.log("Modulus (Remainder):", a % b)
+console.log("Exponent (a²):", a ** 2)
+console.log("Exponent (b²):", b ** 2)
+console.log("Post-increment (a++):", a++)
+console.log("Pre-increment (++b):", ++b)
+console.log("Post-decrement (a--):", a--)
+console.log("Pre-decrement (--b):", --b)
